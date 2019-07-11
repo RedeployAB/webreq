ec9e1c72479146b1be100e5515c28006f2b3b9ec
Karl Wallenius <karl.wallenius@gmail.com>
2019-07-12 00:58:36 +0200

    Updated declaration file, formatting and layout

--- lib/webreq.d.ts
+++ lib/webreq.d.ts
@@ -1,75 +1,3 @@
-interface Buffer {}
-
-class Response {
-    /** Status code of the response */
-    statusCode: number;
-    /** Headers of the response */
-    headers: object;
-    /** Body of the response */
-    body: string|object;
-}
-
-declare interface Agent {
-    /** Keep sockets around for future requests. */
-    keepAlive?: boolean;
-    /** Specifies initial delay for Keep-Alive packets in use with the keepAlive option. */
-    keepAliveMsecs?: number;
-    /**  Maximum number of sockets to allow per host. */
-    maxSockets?: number;
-    /** Maximum number of sockets to leave open if keepAlive is true. */
-    maxFreeSockets?: number;
-    /** Socket timeout in milliseconds. */
-    timeout?: number;
-}
-
-declare interface Certificate {
-    /** Override the trusted CA certificates. */
-    ca?: string[] | Buffer[];
-    /** Certificate chains in PEM format. */
-    cert?: string | string[] | Buffer | Buffer[];
-    /** Private keys in PEM format. If encrypted use together with passphrase. */
-    key?: string | string[] | Buffer | Buffer[];
-    /** Shared passphrase for a private key and/or PFX. */
-    passphrase?: string;
-    /** PFX pr PKCS12 encoded private key and certificate chain. */
-    pfx?: string[] | Buffer[];
-}
-
-declare interface RequestOptions  {
-    /** HTTP method of the request. Default is GET. */
-    method?: string;
-    /** Headers of the request. */
-    headers?: object;
-    /** Data to send with the request. */
-    body?: string | object;
-    /** If true it will check the mime type of the response and output the results accordingly. Default is true.*/
-    parse?: boolean;
-    /** If true it will follow redirects found in the 'location' header. */
-    followRedirects?: boolean;
-    /** Maximum amount of redirects. */
-    maxRedirects?: number;
-    /** When used in a GET request for downloads, it is used as the output path for a file. */
-    path?: string;
-    /** 
-     * Options object for agent for this request.
-     * @param {boolean} [keepAlive] Keep sockets around for future requests.
-     * @param {number} [keepAliveMsecs] Specifies initial delay for Keep-Alive packets in use with the keepAlive option.
-     * @param {number} [maxSockets] Maximum number of sockets to allow per host.
-     * @param {number} [maxFreeSockets] Maximum number of sockets to leave open if keepAlive is true.
-     * @param {number} [timeout] Socket timeout in milliseconds.
-     */
-    agent?: Agent;
-    /** 
-     * Certificate options for the request.
-     * @param {string[] | Buffer[]} [ca] Override the trusted CA certificates.
-     * @param {string[] | Buffer[]} [cert] Certificate chains in PEM format.
-     * @param {string[] | Buffer[]} [key] Private keys in PEM format. If encrypted use together with passphrase.
-     * @param {string} [passphrase] Shared passphrase for a private key and/or PFX.
-     * @param {string[] | Buffer[]} [pfx] PFX pr PKCS12 encoded private key and certificate chain.
-     */
-    certificate: Certificate;
-}
-
 declare class WebReq {
     constructor();
     /** Returns response as a stream */
@@ -89,9 +17,13 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    request(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    request(
+        uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
 
     /**
     * Performs a HTTP/HTTPS GET request.
@@ -101,9 +33,12 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    get(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    get(uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
     
     /**
     * Performs a HTTP/HTTPS POST request.
@@ -113,9 +48,13 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    post(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    post(
+        uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
 
     /**
     * Performs a HTTP/HTTPS PUT request.
@@ -125,9 +64,13 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    put(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    put(
+        uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
 
     /**
     * Performs a HTTP/HTTPS PATCH request.
@@ -137,9 +80,13 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    patch(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    patch(
+        uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
 
     /**
     * Performs a HTTP/HTTPS DELETE request.
@@ -149,9 +96,13 @@ declare class WebReq {
     * @param {string} uri URI of the request.
     * @param {object} [options] Options for the request.
     * @param {function} [callback] Optional callback function.
-    * @return {function|Promise<Response>}
+    * @return {function|Promise<WebReq.Response>}
     */
-    delete(uri: string, options?: RequestOptions, callback?: (err: Error, res: Response) => void): Promise<Response | Error> | void;
+    delete(
+        uri: string,
+        options?: WebReq.RequestOptions,
+        callback?: (err: Error, res: WebReq.Response) => void
+    ): Promise<WebReq.Response | Error> | void;
 
    /**
    * Configures the global agent for the requests.
@@ -164,9 +115,79 @@ declare class WebReq {
     globalAgent(options: object): void;
 }
 
-//export let webreq = webreq;
-let webreq = new WebReq();
-export = webreq;
-export let Agent = Agent;
-export let Certificate = Certificate;
-export let RequestOptions = RequestOptions;
+interface Buffer {}
+
+declare namespace WebReq {
+
+    declare class Response {
+        /** Status code of the response */
+        statusCode: number;
+        /** Headers of the response */
+        headers: object;
+        /** Body of the response */
+        body: string|object;
+    }
+
+    declare interface Agent {
+        /** Keep sockets around for future requests. */
+        keepAlive?: boolean;
+        /** Specifies initial delay for Keep-Alive packets in use with the keepAlive option. */
+        keepAliveMsecs?: number;
+        /**  Maximum number of sockets to allow per host. */
+        maxSockets?: number;
+        /** Maximum number of sockets to leave open if keepAlive is true. */
+        maxFreeSockets?: number;
+        /** Socket timeout in milliseconds. */
+        timeout?: number;
+    }
+
+    declare interface Certificate {
+        /** Override the trusted CA certificates. */
+        ca?: string[] | Buffer[];
+        /** Certificate chains in PEM format. */
+        cert?: string | string[] | Buffer | Buffer[];
+        /** Private keys in PEM format. If encrypted use together with passphrase. */
+        key?: string | string[] | Buffer | Buffer[];
+        /** Shared passphrase for a private key and/or PFX. */
+        passphrase?: string;
+        /** PFX pr PKCS12 encoded private key and certificate chain. */
+        pfx?: string[] | Buffer[];
+    }
+
+    declare interface RequestOptions  {
+        /** HTTP method of the request. Default is GET. */
+        method?: string;
+        /** Headers of the request. */
+        headers?: object;
+        /** Data to send with the request. */
+        body?: string | object;
+        /** If true it will check the mime type of the response and output the results accordingly. Default is true.*/
+        parse?: boolean;
+        /** If true it will follow redirects found in the 'location' header. */
+        followRedirects?: boolean;
+        /** Maximum amount of redirects. */
+        maxRedirects?: number;
+        /** When used in a GET request for downloads, it is used as the output path for a file. */
+        path?: string;
+        /**
+         * Options object for agent for this request.
+         * @param {boolean} [keepAlive] Keep sockets around for future requests.
+         * @param {number} [keepAliveMsecs] Specifies initial delay for Keep-Alive packets in use with the keepAlive option.
+         * @param {number} [maxSockets] Maximum number of sockets to allow per host.
+         * @param {number} [maxFreeSockets] Maximum number of sockets to leave open if keepAlive is true.
+         * @param {number} [timeout] Socket timeout in milliseconds.
+         */
+        agent?: Agent;
+        /**
+         * Certificate options for the request.
+         * @param {string[] | Buffer[]} [ca] Override the trusted CA certificates.
+         * @param {string[] | Buffer[]} [cert] Certificate chains in PEM format.
+         * @param {string[] | Buffer[]} [key] Private keys in PEM format. If encrypted use together with passphrase.
+         * @param {string} [passphrase] Shared passphrase for a private key and/or PFX.
+         * @param {string[] | Buffer[]} [pfx] PFX pr PKCS12 encoded private key and certificate chain.
+         */
+        certificate?: Certificate;
+    }
+}
+
+export = new WebReq();
